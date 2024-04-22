fccb12567793560fb70d2c081c1ff189e3adb348
wirayuda <wirayuda233@gmail.com>
2024-04-22 21:07:23 +0800

    refactor

--- application/src/app.controller.ts
+++ application/src/app.controller.ts
@@ -1,12 +1,9 @@
-import clerkClient from '@clerk/clerk-sdk-node';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   async getUsers() {
-    return await clerkClient.clients.getClient(
-      'app_2aVi6tWNWvg5UZvTLoyDDAZsgvc',
-    );
+    return 'users';
   }
 }
