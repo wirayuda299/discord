7fbe83e3b07bc54775187807a6f9ca8ecc5e7365
wirayuda <wirayuda233@gmail.com>
2024-05-25 07:39:53 +0800

    use fastify

--- application/src/common/errorhandler/error-handler.ts
+++ application/src/common/errorhandler/error-handler.ts
@@ -1,20 +1,21 @@
 import {
   ArgumentsHost,
-  BadRequestException,
   Catch,
   ExceptionFilter,
   HttpException,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { ZodError } from 'zod';
 
 @Catch(ZodError, HttpException, NotFoundException, BadRequestException)
 export class ValidationFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
-    const response = host.switchToHttp().getResponse();
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
 
     if (exception instanceof HttpException) {
-      return response.status(exception.getStatus()).json({
+      return response.status(exception.getStatus()).send({
         messages: exception.getResponse(),
         error: true,
         code: exception.getStatus(),
@@ -23,19 +24,19 @@ export class ValidationFilter implements ExceptionFilter {
       exception instanceof ZodError ||
       exception instanceof BadRequestException
     ) {
-      return response.status(400).json({
+      return response.status(400).send({
         messages: 'Invalid data',
         error: true,
         code: 400,
       });
     } else if (exception instanceof NotFoundException) {
-      return response.status(404).json({
+      return response.status(404).send({
         code: 404,
         messages: exception.message,
         error: true,
       });
     } else {
-      return response.status(500).json({
+      return response.status(500).send({
         code: 500,
         messages: exception.message,
         error: true,
--- application/src/main.ts
+++ application/src/main.ts
@@ -4,16 +4,23 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 import { ValidationFilter } from './common/errorhandler/error-handler';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: process.env.ORIGIN || '*',
-      methods: 'GET, POST, PATCH, PUT, DELETE',
-      allowedHeaders: ['content-type', 'Cookie'],
-      credentials: true,
-    },
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: {
+        origin: process.env.ORIGIN || '*',
+        methods: 'GET, POST, PATCH, PUT, DELETE',
+        allowedHeaders: ['content-type', 'Cookie'],
+        credentials: true,
+      },
+    }
+  );
   app.use(cookieParser());
   app.use(compression());
   app.enableShutdownHooks();
