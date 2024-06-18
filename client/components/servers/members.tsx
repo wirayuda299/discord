e51b1d9cfd1f38d5b3839428d278f3882076fc04
wirayuda <wirayuda233@gmail.com>
2024-06-18 22:56:36 +0800

    create UI for each member menu

--- client/components/servers/members.tsx
+++ client/components/servers/members.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import type { ReactNode } from 'react';
-import { X } from 'lucide-react';
+import { useState, type ReactNode } from 'react';
+import { Ellipsis, X } from 'lucide-react';
 
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from '../ui/sheet';
 import useServerMembers from '@/hooks/useServerMember';
+import { cn } from '@/lib/utils';
 
 export default function ServersMembers({
   serverId,
@@ -14,6 +15,8 @@ export default function ServersMembers({
   children: ReactNode;
 }) {
   const { data, error, isLoading } = useServerMembers(serverId);
+  const [isOpen, setIsOpen] = useState(false)
+
 
   if (isLoading)
     return (
@@ -42,37 +45,57 @@ export default function ServersMembers({
           {data?.map((member) => (
             <li
               key={member.id}
-              className='flex-center gap-2 rounded-md p-2 hover:bg-foreground hover:brightness-110'
+              className='flex-center justify-between gap-2 rounded-md p-2 hover:bg-foreground group hover:brightness-110'
             >
-              <Image
-                src={member.serverProfile.avatar}
-                width={50}
-                height={50}
-                alt={member.username}
-                className='size-12 rounded-full object-cover'
-              />
-              <Link href={'#'}>
-                <div className='flex-center gap-2'>
-                  {member.role && member.role.icon && (
-                    <Image
-                      src={member.role.icon}
-                      width={20}
-                      height={20}
-                      alt='icon'
-                      className='size-4 rounded-full object-cover'
-                    />
-                  )}
-                  <h3
-                    className='text-xl font-semibold'
-                    style={{
-                      ...(member.role && { color: member.role.role_color }),
-                    }}
-                  >
-                    {member.username}
-                  </h3>
-                </div>
-                {member.role && <p className='text-xs'>{member.role.name}</p>}
-              </Link>
+              <div className='flex-center gap-2'>
+
+                <Image
+                  src={member.serverProfile.avatar}
+                  width={50}
+                  height={50}
+                  alt={member.username}
+                  className='size-12 rounded-full object-cover'
+                />
+                <Link href={'#'}>
+                  <div className='flex-center gap-2'>
+                    {member.role && member.role.icon && (
+                      <Image
+                        src={member.role.icon}
+                        width={20}
+                        height={20}
+                        alt='icon'
+                        className='size-4 rounded-full object-cover'
+                      />
+                    )}
+                    <h3
+                      className='text-xl font-semibold'
+                      style={{
+                        ...(member.role && { color: member.role.role_color }),
+                      }}
+                    >
+                      {member.username}
+                    </h3>
+                  </div>
+                  {member.role && <p className='text-xs'>{member.role.name}</p>}
+                </Link>
+
+
+              </div>
+              <div className='relative'>
+                <button className=' group-hover:opacity-100 opacity-0' onClick={() => setIsOpen(prev => !prev)}>
+                  <Ellipsis className='text-gray-2' />
+                </button>
+
+                <ul className={cn('w-full rounded space-y-3 min-w-56 bg-black text-white absolute top-7 p-2 right-0 hidden', isOpen && "block")}>
+                  <li className='text-sm rounded-sm hover:bg-background/50 p-1 cursor-pointer'>
+
+                    Ban Member
+                  </li>
+                  <li className='text-sm hover:bg-background/50 rounded-sm p-1 cursor-pointer'>
+                    Kick member
+                  </li>
+                </ul>
+              </div>
             </li>
           ))}
         </ul>
