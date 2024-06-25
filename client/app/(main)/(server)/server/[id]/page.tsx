6e46bb2a4f512a06021a31b3c50cec9d268e5f3e
wirayuda <wirayuda233@gmail.com>
2024-06-26 00:15:34 +0800

    add check for banned members in server detail

--- client/app/(main)/(server)/server/[id]/page.tsx
+++ client/app/(main)/(server)/server/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { getBannedMembers } from "@/helper/members";
 import { isMemberOrAdmin } from "@/helper/server";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -14,8 +15,14 @@ export default async function ServerDetail({ params }: Props) {
     redirect('/sign-in')
   }
   const { isAuthor, isMember } = await isMemberOrAdmin(userId, params.id)
+  const bannedMembers = await getBannedMembers(params.id)
+  const userIds = bannedMembers.map(member => member.id)
+
+  if (userIds.includes(userId)) {
+    redirect('/direct-messages')
+  }
   if (!isAuthor && !isMember) {
     redirect('/direct-messages')
   }
-  return null
+  return <div></div>
 }
