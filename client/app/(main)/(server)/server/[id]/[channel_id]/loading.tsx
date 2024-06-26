833c933f91350b8c269f9de0276413cab54e16e4
wirayuda <wirayuda233@gmail.com>
2024-06-26 17:12:16 +0800

    remove unecesary loader

--- client/app/(main)/(server)/server/[id]/[channel_id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import PulseLoader from "@/components/shared/pulse-loader";
-
-export default function Loader() {
-	return (
-		<div className="fixed min-h-dvh z-50 w-full max-h-dvh md:static md:min-h-screen md:max-h-screen">
-			<PulseLoader />
-		</div>
-	)
-}
